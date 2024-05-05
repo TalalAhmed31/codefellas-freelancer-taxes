aff0c0cba0eb377052ddebce17e1a9b8d851d50a
Talal Ahmed <talalsidd31@gmail.com>
2024-05-05 07:00:03 +0500

    update2

--- app.py
+++ app.py
@@ -79,26 +79,6 @@ class GeminiEmbeddingFunction(EmbeddingFunction):
                                    title=title)["embedding"]
 
 import chromadb
-from typing import List
-def create_chroma_db(documents:List, path:str, name:str):
-    """
-    Creates a Chroma database using the provided documents, path, and collection name.
-
-    Parameters:
-    - documents: An iterable of documents to be added to the Chroma database.
-    - path (str): The path where the Chroma database will be stored.
-    - name (str): The name of the collection within the Chroma database.
-
-    Returns:
-    - Tuple[chromadb.Collection, str]: A tuple containing the created Chroma Collection and its name.
-    """
-    chroma_client = chromadb.PersistentClient(path=path)
-    db = chroma_client.create_collection(name=name, embedding_function=GeminiEmbeddingFunction())
-
-    for i, d in enumerate(documents):
-        db.add(documents=d, ids=str(i))
-
-    return db, name
 
 # db,name =create_chroma_db(documents=chunked_text, 
 #                           path="./", #replace with your path
